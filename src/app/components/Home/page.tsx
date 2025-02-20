25f5d62027b0548b725ecfb3e48d14a42a554a36
Tanaka878 <musungaretanaka@gmail.com>
2025-02-20 18:56:36 +0200

    project initialisation

---
+++ src/app/components/Home/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1 className='text-slate-950 md:text-3xl sm:text-xl'>Hands-Free AI</h1>
+
+
+      
+    </div>
+  )
+}
+
+export default HomePage
