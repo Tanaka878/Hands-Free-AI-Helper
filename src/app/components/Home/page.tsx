0b60d4a6093d6d72e38f87c229db7782c9f7a463
Tanaka878 <musungaretanaka@gmail.com>
2025-02-21 11:21:07 +0200

    commit

--- src/app/components/Home/page.tsx
+++ src/app/components/Home/page.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-        <h1 className='text-slate-950 md:text-3xl sm:text-xl'>Hands-Free AI</h1>
+    <div className="flex justify-between items-center w-full px-4">
+          <h1 className="text-slate-950 md:text-3xl sm:text-xl flex justify-center">Hands-Free AI</h1>
+          <nav className="flex justify-end">
+            <h1 className="rounded-full bg-black text-white w-10 h-10 flex items-center justify-center">
+              T
+            </h1>
+          </nav>
+
+          <input />
+</div>
 
 
-      
-    </div>
   )
 }
 
